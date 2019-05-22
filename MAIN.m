4cda9418c822738e7b46f40c9785d7f6a32a2019
yiucla <35941111+yiucla@users.noreply.github.com>
2019-05-22 11:05:54 +0800

    Update MAIN.m

--- MAIN.m
+++ MAIN.m
@@ -23,10 +23,16 @@ beta_ini = fit.beta(:,find(fit.lambda==lambda));
 
 %% BJASS
 [S,beta_S,l] = KSV(X,Y,delta,beta_ini,d); 
+beta_S = Refit1(X(:,S),Y,delta);
 [Snew,beta_Snew,l_new] = BJASS(X,Y,delta,beta_ini,d,S,beta_S);
+beta_Snew = Refit2(X(:,Snew),Y,delta,beta_S);
 [Snew2,beta_Snew2,l_new2] = BJASS(X,Y,delta,beta_ini,d,Snew,beta_Snew);
+beta_Snew2 = Refit2(X(:,Snew2),Y,delta,beta_Snew);
 [Snew3,beta_Snew3,l_new3] = BJASS(X,Y,delta,beta_ini,d,Snew2,beta_Snew2);
+beta_Snew3 = Refit2(X(:,Snew3),Y,delta,beta_Snew2);
 [Snew4,beta_Snew4,l_new4] = BJASS(X,Y,delta,beta_ini,d,Snew3,beta_Snew3);
+beta_Snew4 = Refit2(X(:,Snew4),Y,delta,beta_Snew3);
 [Snew5,beta_Snew5,l_new5] = BJASS(X,Y,delta,beta_ini,d,Snew4,beta_Snew4);
+beta_Snew5 = Refit2(X(:,Snew5),Y,delta,beta_Snew4);
 
 
